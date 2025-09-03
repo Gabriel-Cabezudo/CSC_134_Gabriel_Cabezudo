3f5be0fd05d82f8c4e02f95a72d92db9c892d8b0
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-03 11:28:35 -0400

    divide by 0 code

---
+++ divides_by_0.cpp
@@ -0,0 +1,22 @@
+//@author: Gabriel Cabezudo
+//@date: 08/27/2025
+//This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 5;
+    int quotient = num1 / 0;
+
+    //this prints out the quotient of the two numbers from above.
+    std::cout << quotient << std::endl;
+
+
+    return 0;
+}
