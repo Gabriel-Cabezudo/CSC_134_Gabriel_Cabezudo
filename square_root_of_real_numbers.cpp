3514b24acc85092696661a28e8ab55dfe0dfca11
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-03 11:31:16 -0400

    square root of real numbers

---
+++ square_root_of_real_numbers.cpp
@@ -0,0 +1,22 @@
+//@author: Gabriel Cabezudo
+//@date: 08/27/2025
+//This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+#include <cmath> // include cmath for sqrt function
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 5;
+    sqrt(num1);
+
+    //this prints out the square root of the two numbers from above.
+    std::cout << sqrt(num1) << std::endl;
+
+    return 0;
+}
