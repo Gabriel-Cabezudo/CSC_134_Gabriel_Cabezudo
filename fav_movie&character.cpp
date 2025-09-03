d15be47faa72f54eee286c2d61238726044d17c6
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-03 11:47:39 -0400

    fav movie and characters

---
+++ fav_movie&character.cpp
@@ -0,0 +1,15 @@
+//@author: Gabriel Cabezudo
+//@date: 09/3/2025
+//This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    
+}
