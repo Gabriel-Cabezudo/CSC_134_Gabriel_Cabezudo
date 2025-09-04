cb56a39f14361966e862945d1898aa305e90aad8
Gabriel Cabezudo <cabezudogo200@mail.sandhills.edu>
2025-09-03 20:36:33 -0400

    fav movie and characters

--- fav_movie&character.cpp
+++ fav_movie&character.cpp
@@ -11,5 +11,16 @@ using namespace std;
 //define the main() function/method
 int main()
 {
-    
+    //favorite movie and characters
+    string fav_movie = "10 Things I Hate About You";
+    string character1 = "Kat";
+    string character2 = "Patrick";
+    string character3 = "Bianca";
+    string character4 = "Cameron";
+
+    //print out the favorite movie and characters
+    std::cout << "My favorite movie is: " << fav_movie << std::endl;
+    std::cout << "My favorite characters are: " << character1 << ", " << character2 << ", " << character3 << ", and " << character4 << "." << std::endl;
+
+    return 0;
 }
