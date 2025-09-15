f4fc43b08d3bf394b1eb2b1319fa4e5fad93e5be
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-15 12:05:53 -0400

    new

---
+++ angle_of_righttriangle.cpp
@@ -0,0 +1,16 @@
+//author: Gabriel Cabezudo
+//date: 9/15/2025
+//This program calculates the angle of a right triangle given the lengths of the opposite and adjacent sides.
+
+//include necessary libraries
+#include <iostream>
+#include <cmath> //for atan and degrees conversion
+
+//use the standard namespace
+using namespace std;
+
+int main()
+{
+    //declare variables
+    double opposite, adjacent, angle;
+}
---
+++ infinite_loop.cpp
@@ -0,0 +1,23 @@
+//author: Gabriel Cabezudo
+//date: 9/15/2025
+//This program demonstrates an infinite loop using a while loop.
+
+//include necessary libraries
+#include <iostream>
+#include <cmath> //for mathematical functions
+
+//use the standard namespace
+using namespace std;
+
+int main()
+{
+    //declare variables
+    int count = 0;
+
+    //infinite loop using while
+    while (true) // this will always be true
+    {
+        cout << "This is infinite\n";
+    }
+    return 0;
+}
