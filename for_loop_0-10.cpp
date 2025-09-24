2d1cb4dea4d79ccb070b0b11f27c8a00d554f535
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-09-24 11:33:08 -0400

    week 5

---
+++ for_loop_0-10.cpp
@@ -0,0 +1,22 @@
+//author: Gabriel Cabezudo
+//date: 09/24/2025
+//This program implements a for loop that counts from 0 to 10
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+int main()
+{
+    //for loop that counts from 0 to 10
+    for (int count = 0; count <= 10; count++)
+    {
+        //print out each iteration of the loop
+        std::cout << count << std::endl;
+
+    }
+    return 0;
+
+}
