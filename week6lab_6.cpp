f1fbe07e41fed81d5a5799c6fa9bb31b507ca692
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-10-01 11:48:13 -0400

    week 6

---
+++ week6lab_6.cpp
@@ -0,0 +1,19 @@
+//author: Gabriel Cabezudo
+//date: 10/1/2025
+//This program initializes two variables, sumOp and adder, and uses a for-loop to loop 50 times and sums the loop counter and adder variable. Print sumOp through each iteration
+
+//include libary
+#include <iostream>
+#include <cmath>
+
+//set standard namespace
+using namespace std;
+
+main () 
+{
+    //initialize variables sumOp and adder
+    int sumOp = 0;
+    int adder =10;
+
+    //create for loop to loop 50 times and sum the loop counter and adder variable
+}
